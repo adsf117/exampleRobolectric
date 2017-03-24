2a8b6f420e883fa6f4395ccc15db7d27273a2c23
ads.f117 <ads.f117@gmail.com>
2017-03-24 11:53:09 -0500

    test check second activity

--- app/src/main/java/com/puzzlebench/katarobolectric/MainActivity.java
+++ app/src/main/java/com/puzzlebench/katarobolectric/MainActivity.java
@@ -1,18 +1,35 @@
 package com.puzzlebench.katarobolectric;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = (TextView)findViewById(R.id.texview);
         textView.setText("H");
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSecondActivity();
+            }
+        });
+    }
+
+    public void openSecondActivity()
+    {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
     }
 
     public void getText()
--- app/src/test/java/com/puzzlebench/katarobolectric/MainActivityTest.java
+++ app/src/test/java/com/puzzlebench/katarobolectric/MainActivityTest.java
@@ -1,5 +1,8 @@
 package com.puzzlebench.katarobolectric;
 
+import android.content.Intent;
+
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.junit.Before;
@@ -9,8 +12,16 @@ import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
+import org.robolectric.shadows.ShadowActivity;
+import org.robolectric.shadows.ShadowApplication;
+import org.robolectric.shadows.ShadowIntent;
+
+
+import static junit.framework.TestCase.assertEquals;
 import static org.assertj.core.api.Java6Assertions.*;
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertNotNull;
+import static org.robolectric.Shadows.shadowOf;
 
 
 /**
@@ -20,13 +31,18 @@ import static org.junit.Assert.assertNotNull;
 @RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 21)
 public class MainActivityTest {
+
+
     private MainActivity activity;
     private TextView textView;
+    private Button button;
 
     @Before
     public void setUp() throws Exception {
         activity = Robolectric.buildActivity(MainActivity.class).create().get();
         textView = (TextView)activity.findViewById(R.id.texview);
+        button = (Button)activity.findViewById(R.id.button);
+
 
     }
 
@@ -34,8 +50,19 @@ public class MainActivityTest {
     public void onCreate() throws Exception {
 
         assertNotNull(activity);
-        assertThat(textView.getText().toString()).isEqualTo("Hola");
+        assertThat(textView.getText().toString()).isEqualTo("H");
+
+
 
     }
+    @Test
+    public void openSecondActivity() throws Exception {
+        activity.openSecondActivity();
+        Intent startedActivity = ShadowApplication.getInstance().peekNextStartedActivity();
+        assertEquals(SecondActivity.class.getName(), startedActivity.getComponent().getClassName());
+
+    }
+
+
 
 }
